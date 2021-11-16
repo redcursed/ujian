614a07c0457aa72ecbacf1113e2a463f50f87185
redcursed <91407960+redcursed@users.noreply.github.com>
2021-11-16 11:42:46 +0700

    Update ujian.py

--- ujian.py
+++ ujian.py
@@ -1,22 +1,22 @@
 barang = []
 tujuan = []
 pengiriman = []
-    
+
 loop = True
- 
-print('================== paket ==============')
-print('1. Tambah Daftar barang kiriman')
+
+print('================== Paket ==============')
+print('1. Tambah Daftar Parang Kiriman')
 print('2. List Beli')
 print('3. Keluar')
 print('======================================')
 while (loop):
-       
-    paket = input('Masukan pilihan = ')
- 
+
+    paket = input('Masukan Pilihan = ')
+
     if paket == "1":
         print("==============")
-        print('Silahkan Masukan paket yang anda ingin')
-        print('====================== Daftar beli ===================')
+        print('Silahkan Masukan Paket yang Anda Ingin')
+        print('====================== Daftar  ===================')
         tambah_paket = (input('Mau Kirim Apa :  '))
         tambah_tujuan = (input('Masukan Tujuan Anda :  '))
         tambah_pengiriman = (input('Mau Berapa Lama :  '))
@@ -25,13 +25,14 @@ while (loop):
         pengiriman.append(tambah_pengiriman)
     elif paket == "2":
         print('===== List Belanja =====')
-        for i in range(0,len(barang)):
+        for i in range(0, len(barang)):
             info_barang = barang[i]
             info_tujuan = tujuan[i]
             info_pengiriman = pengiriman[i]
-            print(f'anda mengirim {info_barang} dengan tujuan {info_tujuan} dan Estimasi waktu {info_pengiriman}')
-    elif paket== "3":
-        quit()
+            print(
+                f'anda mengirim {info_barang} dengan tujuan {info_tujuan} dan Estimasi waktu {info_pengiriman}')
+    elif paket == "3":
+        print('terimakasih telah memilih layaanan kami, permintaan anda akan kamu proses')
+        break
     else:
         print("Masukan Menu Dengan Benar")
-       
